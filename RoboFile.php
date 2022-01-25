3570d22ae20ff809e7b9bfc7d9379ee9e9532fd2
Kelvin Mo <kmo@users.sourceforge.net>
2022-01-25 12:15:32 +1100

    Fix bug in RoboFile.php

--- RoboFile.php
+++ RoboFile.php
@@ -48,7 +48,7 @@ class RoboFile extends \Robo\Tasks {
             ->run();
 
         // 6. See if module exists in the right location
-        $colleciton->addCode(function() use ($workspace_dir) {
+        $collection->addCode(function() use ($workspace_dir) {
             $expected_file = $workspace_dir . '/site/test/src/TestModule/TestModule.php';
             if (!file_exists($expected_file)) return 1;
         });
